2596e958470becdf104ba48008094690f2aadb6c
Denis-Pankov <angelman777@mail.ru>
2023-01-03 16:23:53 +0300

    gh-pages added

--- src/layout/Footer.jsx
+++ src/layout/Footer.jsx
@@ -4,7 +4,7 @@ function Footer() {
       <div className="footer-copyright">
         <div className="container">
           © {new Date().getFullYear()} Copyright Text
-          <a className="grey-text text-lighten-4 right" href="#!">
+          <a className="grey-text text-lighten-4 right" rel="noreferrer" target='_blank' href="https://github.com/Denis-Pankov/ReactShop-2">
             Repo
           </a>
         </div>
--- src/layout/Header.jsx
+++ src/layout/Header.jsx
@@ -4,12 +4,12 @@ function Header() {
   return (
     <nav className="purple lighten-3">
       <div className="nav-wrapper">
-        <a href="!#" className="brand-logo">
+        <a href="/" className="brand-logo">
           React SHOP
         </a>
         <ul id="nav-mobile" className="right hide-on-med-and-down">
           <li>
-            <a href="https://github.com/Denis-Pankov/ReactShop-2">Repo</a>
+            <a href="https://github.com/Denis-Pankov/ReactShop-2" rel="noreferrer" target='_blank'>Repo</a>
           </li>
         </ul>
       </div>
